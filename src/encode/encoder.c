2b8fcf718f540f38bbcb1fbfca003302959b28ea
nathan-moore-97 <nathanmoore994@gmail.com>
2021-04-18 23:51:24 -0400

    Stubbing out output PNG to a copy of the input PNG, re-adding decoder to Makefile, and unignoring Makefile from the gitignore

--- src/encode/encoder.c
+++ src/encode/encoder.c
@@ -10,7 +10,7 @@
 
 
 void Usage() {
-    puts("./encoder IMG SECRET [OUTPUT_FILENAME] ");
+    puts("./encoder <PATH_TO_IMG> <PATH_TO_SECRET> <OUTPUT_FILENAME>");
 }
 
 bool is_valid_png(int image_fd) {
@@ -22,13 +22,14 @@ bool is_valid_png(int image_fd) {
 
 int main(int argc, char *argv[]) {
     // User must submit an IMG and Secret to program
-    if(argc < 3) {
+    if(argc < 4) {
         Usage();
         exit(1);
     }
     
     int image_fd;
     int secret_fd;
+    int output_fd;
 
     image_fd = open(argv[1], O_RDONLY);
     if(image_fd < 0) {
@@ -43,14 +44,25 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    if(!encode_secret_to_png(image_fd, secret_fd, -1, 2)) {
+    output_fd = open(argv[3], O_CREAT | O_RDWR, (S_IRUSR | S_IWUSR));
+    if(output_fd < 0) {
+        perror("Failed to create output file");
         close(image_fd);
         close(secret_fd);
         exit(1);
     }
 
+
+    if(!encode_secret_to_png(image_fd, secret_fd, output_fd, 2)) {
+        close(image_fd);
+        close(secret_fd);
+        close(output_fd);
+        exit(1);
+    }
+
     close(image_fd);
     close(secret_fd);
+    close(output_fd);
     exit(0);
 }
 
@@ -94,6 +106,19 @@ bool encode_secret_to_png(int image_fd, int secret_fd, int output_fd, int resolu
 
     } while(cur_offset < image_nbytes);
 
+    char output_buffer[image_nbytes];
+    lseek(image_fd, 0, SEEK_SET);
+    lseek(output_fd, 0, SEEK_SET);
+    if(read(image_fd, &output_buffer, image_nbytes) < 0) {
+        perror("Error reading image to buffer");
+        exit(1);
+    }
+   
+    if(write(output_fd, &output_buffer, image_nbytes) < 0) {
+        perror("Error writing image to tmp result file from buffer");
+        exit(1);
+    }
+
     // EVERYTHING SHALL BE DOUBLES!!!
     used_capacity = (((double)secret_nbytes * (double)resolution) / (double)total_dat_len) * 100;
 
@@ -103,8 +128,6 @@ bool encode_secret_to_png(int image_fd, int secret_fd, int output_fd, int resolu
     printf("Encodable bytes:\t\t%d\n", total_dat_len);
     printf("Bytes to encode:\t\t%d\n", secret_nbytes);
     printf("PNG Capacity Used:\t\t %f percent\n", used_capacity);
-
-
 
     return true;
 }
